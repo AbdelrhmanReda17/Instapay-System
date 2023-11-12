f8c5d29975232f85dbadc91dac6bfd6daf3d7b23
abdelrahman atef <102483897+AbdoMan19@users.noreply.github.com>
2023-11-13 00:44:04 +0200

    add bills function

---
+++ src/Bills/ElectricityBills.java
@@ -0,0 +1,23 @@
+package Bills;
+public class ElectricityBills extends Bill {
+
+    private double usageInKWh;
+    public ElectricityBills(int billId, String username, double amount, boolean isPaid, double usageInKWh){
+        super(billId, username, amount ,isPaid);
+        this.usageInKWh = usageInKWh;
+    }
+    public ElectricityBills(){}
+    public void setUsage(double usage){
+        this.usageInKWh = usage;
+
+    }
+
+    @Override
+    public void PrintBillDetails() {
+        System.out.println("Electricity Bill");
+        System.out.println("Usage: " + this.usageInKWh + " kWh");
+        System.out.println("Total Amount: " + this.totalAmount);
+        System.out.println("Is Paid: " + this.isPaid);
+    }
+
+}
