07962f5e349ee3d1bd59ef97aab7b62f970a42fa
Abdelrhman R,Muhammed <11410120210225@stud.cu.edu.eg>
2023-11-14 21:54:20 +0200

    Fix Import bug

--- src/InstapayApplication/Views/Factories/ViewFactory.java
+++ src/InstapayApplication/Views/Factories/ViewFactory.java
@@ -1,15 +1,14 @@
 package InstapayApplication.Views.Factories;
 
 import Entities.User.Account;
-import InstapayApplication.Views.MainMenu.BanckAccountView;
-import InstapayApplication.Views.MainMenu.MainMenu;
-import InstapayApplication.Views.MainMenu.WalletAccountView;
+import InstapayApplication.Views.MainMenu.*;
+
 
 
 public class ViewFactory {
     public static MainMenu CreateMainMenu(Account account) {
         return switch (account.getClass().getSimpleName()) {
-            case "BankAccount" -> new BanckAccountView();
+            case "BankAccount" -> new BankAccountView();
             case "WalletAccount" -> new WalletAccountView();
             default -> null;
         };
